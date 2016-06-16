2130064e300f8d261e878afbc8fc20ba89cbc6b6
Gogoson <380879702@qq.com>
2016-06-16 14:57:03 +0800

    20160616_01

---
+++ javascript-puzzle.js
@@ -0,0 +1,14 @@
+/*
+  44个 Javascript 变态题解析
+
+  收集时间：2016-06-16     
+  英文来自：http://javascript-puzzlers.herokuapp.com/
+  中文来自：https://segmentfault.com/a/1190000005681454?utm_source=tuicool&utm_medium=referral
+*/
+
+/*
+  第一题：
+
+  注意：IE8以下不支持map方法
+*/
+["1", "2", "3"].map(parseInt); //[1, NaN, NaN]
